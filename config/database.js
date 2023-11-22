77d172356294a1ef6c5d781d57bf05824e781a36
cruz-welandi <cruzwelandi@gmail.com>
2023-11-22 02:28:04 +0100

    ajout de sqlite

--- config/database.js
+++ config/database.js
@@ -80,6 +80,12 @@ module.exports = ({ env }) => {
       },
       useNullAsDefault: true,
     },
+    sqlite3: {
+      connection: {
+        filename: './dev.sqlite3'
+      },
+      useNullAsDefault: true
+    }
   };
 
   return {
